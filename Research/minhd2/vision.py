1583ef2ec60e0cc1a8d2a00541bf3d9bae88f1a9
WhiteHoodHacker <github@whitehoodhacker.net>
2021-09-19 23:34:42 -0500

    Add comments

--- Research/minhd2/vision.py
+++ Research/minhd2/vision.py
@@ -2,30 +2,32 @@ import cv2
 import mediapipe as mp
 import time
 
+# Start 640x480 resolution camera capture
 cap = cv2.VideoCapture(1)
 time.sleep(1.000)
 cap.set(3,640)
 cap.set(4,480)
 
+# Initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
 mpDraw = mp.solutions.drawing_utils
 
 while(True):
     success, frame = cap.read()
     #frame = cv2.resize(frame, (1024, 768))
-    # BGR for processing
-    frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
-    processedHands = hands.process(frame)
+    # Mirror camera so handedness is correct
+    frame = cv2.flip(frame, 1)
+    processedHands = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-    # frame information
+    # Frame information
     h, w, c = frame.shape
     x_max = 0
     y_max = 0
     x_min = w
     y_min = h
 
+    # Draw hand landmarks on each frame
     if processedHands.multi_hand_landmarks:
         for idx, handLms in enumerate(processedHands.multi_hand_landmarks):
             lbl = processedHands.multi_handedness[idx].classification[0].label
@@ -44,9 +46,8 @@ while(True):
             cv2.putText(frame, text = lbl, org = (x_min, y_min), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = (0, 255, 0), thickness = 2, lineType = cv2.LINE_AA)
             mpDraw.draw_landmarks(frame, handLms)
 
-    # change colors back
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    cv2.imshow("Video", frame)
+    # Render each frame
+    cv2.imshow("Vision Test", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
