16c216346ccb708db492e369c3c2e35447399bf8
WhiteHoodHacker <github@whitehoodhacker.net>
2021-09-19 22:17:10 -0500

    Bounding box, handedness label, and normal colors

--- Research/minhd2/vision.py
+++ Research/minhd2/vision.py
@@ -15,13 +15,37 @@ mpDraw = mp.solutions.drawing_utils
 while(True):
     success, frame = cap.read()
     #frame = cv2.resize(frame, (1024, 768))
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # BGR for processing
+    frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
     processedHands = hands.process(frame)
 
-    if processedHands.multi_hand_landmarks:
-        for point in processedHands.multi_hand_landmarks:
-            mpDraw.draw_landmarks(frame, point)
+    # frame information
+    h, w, c = frame.shape
+    x_max = 0
+    y_max = 0
+    x_min = w
+    y_min = h
 
+    if processedHands.multi_hand_landmarks:
+        for idx, handLms in enumerate(processedHands.multi_hand_landmarks):
+            lbl = processedHands.multi_handedness[idx].classification[0].label
+            # bounding box code: https://stackoverflow.com/questions/66876906/create-a-rectangle-around-all-the-points-returned-from-mediapipe-hand-landmark-d
+            for lm in handLms.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            #cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            cv2.putText(frame, text = lbl, org = (x_min, y_min), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = (0, 255, 0), thickness = 2, lineType = cv2.LINE_AA)
+            mpDraw.draw_landmarks(frame, handLms)
+
+    # change colors back
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     cv2.imshow("Video", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
